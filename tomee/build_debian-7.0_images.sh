bedd854e83151c3fcb81b2b9fd6286533578040e
rstiller <robert.stiller.mail@googlemail.com>
2014-02-21 11:14:34 +0100

    added tomee

---
+++ tomee/build_debian-7.0_images.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+docker build -t rstiller/tomee:debian-7.0_oracle-jdk-1.7_tomee-1.6.0-plus - < debian-7.0_oracle-jdk-1.7_tomee-1.6.0-plus && \
+docker build -t rstiller/tomee:debian-7.0_oracle-jdk-1.7_tomee-1.5.2-plus - < debian-7.0_oracle-jdk-1.7_tomee-1.5.2-plus && \
+docker build -t rstiller/tomee:debian-7.0_oracle-jdk-1.7_tomee-1.5.1-plus - < debian-7.0_oracle-jdk-1.7_tomee-1.5.1-plus && \
+docker build -t rstiller/tomee:debian-7.0_oracle-jdk-1.7_tomee-1.5.0-plus - < debian-7.0_oracle-jdk-1.7_tomee-1.5.0-plus && \
+docker build -t rstiller/tomee:debian-7.0_oracle-jdk-1.7_tomee-1.0.0-plus - < debian-7.0_oracle-jdk-1.7_tomee-1.0.0-plus && \
+docker build -t rstiller/tomee:debian-7.0_oracle-jdk-1.7_tomee-latest-plus - < debian-7.0_oracle-jdk-1.7_tomee-1.6.0-plus && \
+ \
+docker build -t rstiller/tomee:debian-7.0_oracle-jdk-1.6_tomee-1.6.0-plus - < debian-7.0_oracle-jdk-1.6_tomee-1.6.0-plus && \
+docker build -t rstiller/tomee:debian-7.0_oracle-jdk-1.6_tomee-1.5.2-plus - < debian-7.0_oracle-jdk-1.6_tomee-1.5.2-plus && \
+docker build -t rstiller/tomee:debian-7.0_oracle-jdk-1.6_tomee-1.5.1-plus - < debian-7.0_oracle-jdk-1.6_tomee-1.5.1-plus && \
+docker build -t rstiller/tomee:debian-7.0_oracle-jdk-1.6_tomee-1.5.0-plus - < debian-7.0_oracle-jdk-1.6_tomee-1.5.0-plus && \
+docker build -t rstiller/tomee:debian-7.0_oracle-jdk-1.6_tomee-1.0.0-plus - < debian-7.0_oracle-jdk-1.6_tomee-1.0.0-plus && \
+docker build -t rstiller/tomee:debian-7.0_oracle-jdk-1.6_tomee-latest-plus - < debian-7.0_oracle-jdk-1.6_tomee-1.6.0-plus && \
+ \
+docker build -t rstiller/tomee:debian-7.0_openjdk-jdk-1.7_tomee-1.6.0-plus - < debian-7.0_openjdk-jdk-1.7_tomee-1.6.0-plus && \
+docker build -t rstiller/tomee:debian-7.0_openjdk-jdk-1.7_tomee-1.5.2-plus - < debian-7.0_openjdk-jdk-1.7_tomee-1.5.2-plus && \
+docker build -t rstiller/tomee:debian-7.0_openjdk-jdk-1.7_tomee-1.5.1-plus - < debian-7.0_openjdk-jdk-1.7_tomee-1.5.1-plus && \
+docker build -t rstiller/tomee:debian-7.0_openjdk-jdk-1.7_tomee-1.5.0-plus - < debian-7.0_openjdk-jdk-1.7_tomee-1.5.0-plus && \
+docker build -t rstiller/tomee:debian-7.0_openjdk-jdk-1.7_tomee-1.0.0-plus - < debian-7.0_openjdk-jdk-1.7_tomee-1.0.0-plus && \
+docker build -t rstiller/tomee:debian-7.0_openjdk-jdk-1.7_tomee-latest-plus - < debian-7.0_openjdk-jdk-1.7_tomee-1.6.0-plus && \
+ \
+docker build -t rstiller/tomee:debian-7.0_openjdk-jdk-1.6_tomee-1.6.0-plus - < debian-7.0_openjdk-jdk-1.6_tomee-1.6.0-plus && \
+docker build -t rstiller/tomee:debian-7.0_openjdk-jdk-1.6_tomee-1.5.2-plus - < debian-7.0_openjdk-jdk-1.6_tomee-1.5.2-plus && \
+docker build -t rstiller/tomee:debian-7.0_openjdk-jdk-1.6_tomee-1.5.1-plus - < debian-7.0_openjdk-jdk-1.6_tomee-1.5.1-plus && \
+docker build -t rstiller/tomee:debian-7.0_openjdk-jdk-1.6_tomee-1.5.0-plus - < debian-7.0_openjdk-jdk-1.6_tomee-1.5.0-plus && \
+docker build -t rstiller/tomee:debian-7.0_openjdk-jdk-1.6_tomee-1.0.0-plus - < debian-7.0_openjdk-jdk-1.6_tomee-1.0.0-plus && \
+docker build -t rstiller/tomee:debian-7.0_openjdk-jdk-1.6_tomee-latest-plus - < debian-7.0_openjdk-jdk-1.6_tomee-1.6.0-plus
---
+++ tomee/build_ubuntu-12.04_images.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+docker build -t rstiller/tomee:ubuntu-12.04_oracle-jdk-1.7_tomee-1.6.0-plus - < ubuntu-12.04_oracle-jdk-1.7_tomee-1.6.0-plus && \
+docker build -t rstiller/tomee:ubuntu-12.04_oracle-jdk-1.7_tomee-1.5.2-plus - < ubuntu-12.04_oracle-jdk-1.7_tomee-1.5.2-plus && \
+docker build -t rstiller/tomee:ubuntu-12.04_oracle-jdk-1.7_tomee-1.5.1-plus - < ubuntu-12.04_oracle-jdk-1.7_tomee-1.5.1-plus && \
+docker build -t rstiller/tomee:ubuntu-12.04_oracle-jdk-1.7_tomee-1.5.0-plus - < ubuntu-12.04_oracle-jdk-1.7_tomee-1.5.0-plus && \
+docker build -t rstiller/tomee:ubuntu-12.04_oracle-jdk-1.7_tomee-1.0.0-plus - < ubuntu-12.04_oracle-jdk-1.7_tomee-1.0.0-plus && \
+docker build -t rstiller/tomee:ubuntu-12.04_oracle-jdk-1.7_tomee-latest-plus - < ubuntu-12.04_oracle-jdk-1.7_tomee-1.6.0-plus && \
+ \
+docker build -t rstiller/tomee:ubuntu-12.04_oracle-jdk-1.6_tomee-1.6.0-plus - < ubuntu-12.04_oracle-jdk-1.6_tomee-1.6.0-plus && \
+docker build -t rstiller/tomee:ubuntu-12.04_oracle-jdk-1.6_tomee-1.5.2-plus - < ubuntu-12.04_oracle-jdk-1.6_tomee-1.5.2-plus && \
+docker build -t rstiller/tomee:ubuntu-12.04_oracle-jdk-1.6_tomee-1.5.1-plus - < ubuntu-12.04_oracle-jdk-1.6_tomee-1.5.1-plus && \
+docker build -t rstiller/tomee:ubuntu-12.04_oracle-jdk-1.6_tomee-1.5.0-plus - < ubuntu-12.04_oracle-jdk-1.6_tomee-1.5.0-plus && \
+docker build -t rstiller/tomee:ubuntu-12.04_oracle-jdk-1.6_tomee-1.0.0-plus - < ubuntu-12.04_oracle-jdk-1.6_tomee-1.0.0-plus && \
+docker build -t rstiller/tomee:ubuntu-12.04_oracle-jdk-1.6_tomee-latest-plus - < ubuntu-12.04_oracle-jdk-1.6_tomee-1.6.0-plus && \
+ \
+docker build -t rstiller/tomee:ubuntu-12.04_openjdk-jdk-1.7_tomee-1.6.0-plus - < ubuntu-12.04_openjdk-jdk-1.7_tomee-1.6.0-plus && \
+docker build -t rstiller/tomee:ubuntu-12.04_openjdk-jdk-1.7_tomee-1.5.2-plus - < ubuntu-12.04_openjdk-jdk-1.7_tomee-1.5.2-plus && \
+docker build -t rstiller/tomee:ubuntu-12.04_openjdk-jdk-1.7_tomee-1.5.1-plus - < ubuntu-12.04_openjdk-jdk-1.7_tomee-1.5.1-plus && \
+docker build -t rstiller/tomee:ubuntu-12.04_openjdk-jdk-1.7_tomee-1.5.0-plus - < ubuntu-12.04_openjdk-jdk-1.7_tomee-1.5.0-plus && \
+docker build -t rstiller/tomee:ubuntu-12.04_openjdk-jdk-1.7_tomee-1.0.0-plus - < ubuntu-12.04_openjdk-jdk-1.7_tomee-1.0.0-plus && \
+docker build -t rstiller/tomee:ubuntu-12.04_openjdk-jdk-1.7_tomee-latest-plus - < ubuntu-12.04_openjdk-jdk-1.7_tomee-1.6.0-plus && \
+ \
+docker build -t rstiller/tomee:ubuntu-12.04_openjdk-jdk-1.6_tomee-1.6.0-plus - < ubuntu-12.04_openjdk-jdk-1.6_tomee-1.6.0-plus && \
+docker build -t rstiller/tomee:ubuntu-12.04_openjdk-jdk-1.6_tomee-1.5.2-plus - < ubuntu-12.04_openjdk-jdk-1.6_tomee-1.5.2-plus && \
+docker build -t rstiller/tomee:ubuntu-12.04_openjdk-jdk-1.6_tomee-1.5.1-plus - < ubuntu-12.04_openjdk-jdk-1.6_tomee-1.5.1-plus && \
+docker build -t rstiller/tomee:ubuntu-12.04_openjdk-jdk-1.6_tomee-1.5.0-plus - < ubuntu-12.04_openjdk-jdk-1.6_tomee-1.5.0-plus && \
+docker build -t rstiller/tomee:ubuntu-12.04_openjdk-jdk-1.6_tomee-1.0.0-plus - < ubuntu-12.04_openjdk-jdk-1.6_tomee-1.0.0-plus && \
+docker build -t rstiller/tomee:ubuntu-12.04_openjdk-jdk-1.6_tomee-latest-plus - < ubuntu-12.04_openjdk-jdk-1.6_tomee-1.6.0-plus
