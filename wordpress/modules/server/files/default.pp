aa4ff7037c097afa01b52f230e8fb6d4912eb2c8
rstiller <robert.stiller.mail@googlemail.com>
2014-05-08 00:08:26 +0200

    better run command

--- wordpress/modules/server/files/default.pp
+++ wordpress/modules/server/files/default.pp
@@ -6,3 +6,15 @@ Exec {
 exec { 'postfix_domain':
     command => 'sed \'s/##DOMAIN##/$domain/g /etc/postfix/main.cf\'',
 }
+
+file { '/mails/valiases':         source => 'puppet:///postfix/valiases', }
+exec { 'postmap /mails/valiases': user => root, require => File['/mails/valiases'], }
+
+file { '/mails/vhosts':           source => 'puppet:///postfix/vhosts', }
+exec { 'postmap /mails/vhosts':   user => root, require => File['/mails/vhosts'], }
+
+file { '/mails/vmaps':            source => 'puppet:///postfix/vmaps', }
+exec { 'postmap /mails/vmaps':    user => root, require => File['/mails/vmaps'], }
+
+file { '/mails/passwd':           source => 'puppet:///dovecot/passwd', }
+file { '/mails/users':            source => 'puppet:///dovecot/users', }
--- wordpress/run.sh
+++ wordpress/run.sh
@@ -1,13 +1,16 @@
-docker run -i -t -v \
-    /containers/<DOMAIN>/certs/:/etc/ssl/private:ro \
-    /containers/<DOMAIN>/logs/:/logs:rw \
-    /containers/<DOMAIN>/mails/:/mails:rw \
-    /containers/<DOMAIN>/mysql/:/mysql:rw \
-    /containers/<DOMAIN>/wordpress/:/var/www/wordpress:rw \
-    -e FACTOR_DOMAIN=www.example.com \
-    -c 0.5 \
+container_domain=www.example.com
+container_hostname=www_example_com
+
+docker run -i -t \
+    -v /containers/$container_hostname/certs:/certs:ro \
+    -v /containers/$container_hostname/logs:/logs:rw \
+    -v /containers/$container_hostname/mails:/mails:rw \
+    -v /containers/$container_hostname/mysql:/mysql:rw \
+    -v /containers/$container_hostname/puppet:/puppet:rw \
+    -v /containers/$container_hostname/wordpress:/var/www/wordpress:rw \
+    -e FACTOR_DOMAIN=$container_domain \
     --lxc-conf="lxc.cgroup.cpuset.cpus = 0,1" \
     -m 128m \
-    -h <DOMAIN> \
-    <IMAGE> \
-    /usr/bin/supervisord
+    -h $container_hostname \
+    1c38f66904d4 \
+    /start.sh
