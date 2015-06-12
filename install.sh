74717c56cb54298c7e7f06a54e9834bc926e401b
rstiller <rstiller@users.noreply.github.com>
2015-06-12 22:19:12 +0200

    leaner install script

--- install.sh
+++ install.sh
@@ -1,13 +1,18 @@
-#!/bin/sh
+#!/bin/bash
 
-wget -qO- https://get.docker.com/ | /bin/sh
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
+echo "deb https://get.docker.com/ubuntu docker main" > /etc/apt/sources.list.d/docker.list
+apt-get -qq update
+
+apt-get install -y lxc-docker wget
+
 adduser $USER docker
-
-wget -q --output-document=/usr/local/bin/docker-machine https://github.com/docker/machine/releases/download/v0.2.0/docker-machine_linux-amd64
-chmod +x /usr/local/bin/docker-machine
 
 wget -q --output-document=/usr/local/bin/weave https://github.com/weaveworks/weave/releases/download/latest_release/weave
 chmod a+x /usr/local/bin/weave
 
-/usr/local/bin/weave setup
-/usr/local/bin/weave launch
+DOCKER_HOST_EXPORTED=`grep -c DOCKER_HOST= /etc/environment`
+
+if [ "$DOCKER_HOST_EXPORTED" == "0" ]; then
+    echo "DOCKER_HOST=tcp://127.0.0.1:12375" >> /etc/environment
+fi
